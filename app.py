47aa8177ed3af68f14a6cd45b4f0182249beabf6
JONNY-ME <yohannesmelese4@gmail.com>
2024-12-09 09:03:52 +0400

    3

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 import streamlit as st
 
-st.title('Hello Streamlit')
-st.write("Welcome to my Streamlit app!")
+'''
+App that simulates an interactive calculator
+'''
+
+# Title
+st.title('Interactive Calculator')
+
+# Sidebar
+st.sidebar.title('Calculator')
+num1 = st.sidebar.number_input('Enter first number', value=0)
+num2 = st.sidebar.number_input('Enter second number', value=0)
+
+# Operations
+operations = ['Addition', 'Subtraction', 'Multiplication', 'Division']
+operation = st.sidebar.selectbox('Select operation', operations)
+
+# Calculate
+if operation == 'Addition':
+    result = num1 + num2
+elif operation == 'Subtraction':
+    result = num1 - num2
+elif operation == 'Multiplication':
+    result = num1 * num2
+elif operation == 'Division':
+    if num2 == 0:
+        result = 'Cannot divide by zero'
+    else:
+        result = num1 / num2
