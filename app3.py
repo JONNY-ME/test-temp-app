6b60ca0556f60e484666dc2e2314aa39fa856279
JONNY-ME <yohannesmelese4@gmail.com>
2024-12-10 10:10:11 +0400

    0

---
+++ app3.py
@@ -0,0 +1,64 @@
+import streamlit
+
+"""Portfolio"""
+
+streamlit.title('Portfolio')
+
+# all in the main area
+streamlit.write('''
+## Welcome to my portfolio
+''')
+
+streamlit.write('''
+### About Me
+I am a software developer with experience in Python, JavaScript, and SQL.
+''')
+
+streamlit.write('''
+### Projects
+- [Project 1](
+- [Project 2](
+- [Project 3](
+''')
+
+streamlit.write('''
+### Contact
+- [Email](mailto:
+- [LinkedIn](https://www.linkedin.com/in/)
+''')
+
+streamlit.write('''
+### Resume
+- [Download](https://www.linkedin.com/in/)
+''')
+
+
+# set footer with an input
+footer = """<style>
+a:link , a:visited{
+color: #0078E7;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: #FF0000;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: #f1f1f1;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Created with Love</p>
+</div>
+"""
+streamlit.markdown(footer,unsafe_allow_html=True)
