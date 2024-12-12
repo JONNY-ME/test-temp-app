b82e521a3fe1c2523d098855f129b638d265a758
JONNY-ME <yohannesmelese4@gmail.com>
2024-12-12 07:45:26 +0400

    0

--- app3.py
+++ app3.py
@@ -1,65 +1,56 @@
 import streamlit
-
-"""Portfolio"""
-
-streamlit.title('Portfolio')
-
-# all in the main area
-streamlit.write('''
-## Welcome to my portfolio
-''')
-
-streamlit.write('''
-### About Me
-I am a software developer with experience in Python, JavaScript, and SQL.
-''')
-
-streamlit.write('''
-### Projects
-- AI Chatbot
-- E-commerce website
-- Portfolio website
-''')
-
-streamlit.write('''
-### Resume
-- [Download](https://test-temp-app-nefurdi3fh4f4x3jymgskm.streamlit.app/)
-''')
-
-streamlit.write('''
-### Contact
-- [Email](mailto:dummy@example.com)
-- [LinkedIn](https://www.linkedin.com/in/)
-''')
+import re
 
 
+def clear_page():
+    streamlit.empty()
+    streamlit.write('You can close the tabs now.')
 
-# set footer with an input
-footer = """<style>
-a:link , a:visited{
-color: #0078E7;
-background-color: transparent;
-text-decoration: underline;
-}
 
-a:hover,  a:active {
-color: #FF0000;
-background-color: transparent;
-text-decoration: underline;
-}
+def main_page():
+    streamlit.write(
+    """
+    There are many tabs in your browser. Accessing the webiste needs more resources. 
+    So closing the tabs is necessary and but before closing the tabs you can store all the links in the below. 
+    Add them one by one. Select the "All done" option when you finish addning all the active tabs.
+    """
+    )
 
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: #f1f1f1;
-color: black;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p>Created with Love</p>
-</div>
-"""
-streamlit.markdown(footer,unsafe_allow_html=True)
+
+    links_stored = False
+    def check_links():
+        if not stored_links:
+            streamlit.write('You have not added any links.')
+        else:
+            streamlit.write('You have added the following links:')
+            for i, link in enumerate(stored_links):
+                streamlit.write(f'{i + 1}. {link}')
+
+            global links_stored
+            links_stored = True
+
+
+    button = streamlit.button('All done', on_click=check_links)
+
+
+    n_inputs = 0
+
+    streamlit.write('Enter the links below:')
+    link = streamlit.text_input('Link')
+
+    stored_links = []
+
+    while 1:
+        if link:
+            stored_links.append(link)
+            n_inputs += 1
+            link = streamlit.text_input(f'Link {n_inputs}')
+        if links_stored:
+            break
+    
+    return stored_links
+
+
+if __name__ == '__main__':
+    main_page()
+    clear_page()
