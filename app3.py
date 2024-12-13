00e97ed9579d6a0538ad742faa341a2a2ab0df28
JONNY-ME <yohannesmelese4@gmail.com>
2024-12-13 20:56:29 +0400

    0

--- app3.py
+++ app3.py
@@ -8,15 +8,11 @@ def clear_page():
 
 
 def main_page():
-
-
-
     streamlit.write(
     """
     There are many tabs in your browser. Accessing the website needs more resources.  So closing the tabs is necessary. You may re-open them after accessing the website. But before closing the tabs you have to store all the U-R-Ls in the below.  Add them one by one. Select the "All done" option when you finish adding all the active tabs.
     """, unsafe_allow_html=True, 
     )
-
 
     links_stored = False
     def check_links():
@@ -29,16 +25,11 @@ def main_page():
 
             global links_stored
             links_stored = True
-
-
     button = streamlit.button('All done', on_click=check_links)
 
-
     n_inputs = 0
-
     streamlit.write('Enter the U-R-Ls below:')
     link = streamlit.text_input('U-R-Ls')
-
     stored_links = []
 
     while 1:
@@ -50,7 +41,6 @@ def main_page():
             break
     
     return stored_links
-
 
 if __name__ == '__main__':
     main_page()
